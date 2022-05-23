8dda979ebf2f75577aef26d7cd53ca73a6077cd4
Anand Mahesh <manand881@gmail.com>
2022-05-23 13:09:58 +0530

    delete all users

--- database.py
+++ database.py
@@ -63,7 +63,7 @@ def register_user(connection, uuid, password, company, macid):
 
 def delete_user(connection, uuid, company):
     with connection:
-        connection.execute(DELETE_ALL_USERS, (uuid, company))
+        connection.execute(DELETE_ALL_USERS)
         result = connection.execute(
             CHECK_IF_USER_DELETED, (uuid, company)).fetchone()
         if result is None:
